1940edf1a0210238d52ff0a82499eb37a0b78e10
khairmuhammad <khairmuhammadmemon@gmail.com>
2026-07-04 20:30:09 +0200

    Created account details fetch, update and delete apis for the accounts service

--- accounts/src/main/java/com/khair/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/khair/accounts/service/IAccountsService.java
@@ -4,5 +4,30 @@ import com.khair.accounts.dto.CustomerDto;
 
 public interface IAccountsService {
 
+    /**
+     *
+     * @param customerDto
+     */
     void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber
+     * @return
+     */
+    CustomerDto fetchAccountDetails(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto
+     * @return
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber
+     * @return
+     */
+    boolean deleteAccount(String mobileNumber);
 }
--- accounts/src/main/java/com/khair/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/khair/accounts/service/impl/AccountsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.khair.accounts.service.impl;
 
 import com.khair.accounts.constants.AccountsConstants;
+import com.khair.accounts.dto.AccountsDto;
 import com.khair.accounts.dto.CustomerDto;
 import com.khair.accounts.entity.Accounts;
 import com.khair.accounts.entity.Customer;
 import com.khair.accounts.exception.CustomerAlreadyExistsException;
+import com.khair.accounts.exception.ResourceNotFoundException;
+import com.khair.accounts.mapper.AccountsMapper;
 import com.khair.accounts.mapper.CustomerMapper;
 import com.khair.accounts.repository.AccountsRepository;
 import com.khair.accounts.repository.CustomerRepository;
@@ -51,5 +54,51 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setUpdatedAt(LocalDateTime.now());
         newAccount.setUpdatedBy("System");
         return newAccount;
+    }
+
+    @Override
+    public CustomerDto fetchAccountDetails(String mobileNumber) {
+
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+
+        Accounts account = accountsRepository.findByCustomerId(customer.getCustomerId())
+                .orElseThrow(() -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString()));
+
+        CustomerDto customerDto = CustomerMapper.mapToCustomerDto(customer, new CustomerDto());
+        customerDto.setAccountsDto(AccountsMapper.mapToAccountsDto(account, new AccountsDto()));
+
+        return customerDto;
+    }
+
+    @Override
+    public boolean updateAccount(CustomerDto customerDto) {
+        boolean isUpdated = false;
+        AccountsDto accountsDto = customerDto.getAccountsDto();
+        if (accountsDto != null) {
+            Accounts accounts = accountsRepository.findById(accountsDto.getAccountNumber())
+                    .orElseThrow(() -> new ResourceNotFoundException("Account", "accountNumber", accountsDto.getAccountNumber().toString()));
+
+            AccountsMapper.mapToAccounts(accountsDto, accounts);
+            accounts = accountsRepository.save(accounts);
+
+            Long customerId = accounts.getCustomerId();
+            Customer customer = customerRepository.findById(customerId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Customer", "customerId", customerId.toString()));
+            CustomerMapper.mapToCustomer(customerDto, customer);
+            customerRepository.save(customer);
+            isUpdated = true;
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public boolean deleteAccount(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+
+        accountsRepository.deleteByCustomerId(customer.getCustomerId());
+        customerRepository.deleteById(customer.getCustomerId());
+        return true;
     }
 }
